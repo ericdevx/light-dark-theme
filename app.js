f589143274bfc06c35840ff14670dd19a46500b7
VOIDx-design <77127267+ericdevx@users.noreply.github.com>
2022-02-06 21:54:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// Select The Elements
+var toggle_btn;
+var big_wrapper;
+var hamburguer_menu;
+
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+    hamburguer_menu = document.querySelector(".hamburguer-menu");
+}
+
+const main =  document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation(){
+    // Clone the wrapper
+    dark = !dark;
+    let clone = big_wrapper.cloneNode(true);
+    if(dark){
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    }
+    else {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    document.body.classList.add("stop-scrolling");
+
+    clone.addEventListener("animationend", () => {
+        document.body.classList.remove("stop-scrolling");
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        // Reset variables
+        declare();
+        events();
+    });
+}
+
+function events() {
+    toggle_btn.addEventListener("click", toggleAnimation);
+    hamburguer_menu.addEventListener("click", () => {
+        big_wrapper.classList.toggle("active");
+    })
+}
+
+events();
